36b9e48abb628f361cf8f0274f9e5764b0f8a858
tekeste teklehaimanot <tmehark@gmail.com>
2023-01-31 16:18:29 -0600

    game cards added and certain css effects modified

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 import GAMES_DATA from './games.js';
 
 // create a list of objects to store the data about the games using JSON.parse
-const GAMES_JSON = JSON.parse(GAMES_DATA)
+const GAMES_JSON = JSON.parse(GAMES_DATA);
 
 // remove all child elements from a parent element in the DOM
 function deleteChildElements(parent) {
@@ -30,24 +30,32 @@ function addGamesToPage(games) {
 
     // loop over each item in the data
 
+    for(const game of games){
 
         // create a new div element, which will become the game card
 
-
+const newElement = document.createElement("div");
         // add the class game-card to the list
 
-
+newElement.classList.add("game-card")
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
+     
 
+       
+newElement.innerHTML =`<b>${game.name}</b> <br><u>Description:</u><i style ="color:#9A8B4F"> ${game.description}</i><br><br>
+<img src = "${game.img}" class ="game-img"/>`
 
         // append the game to the games-container
+        gamesContainer.append(newElement);
 
+}
 }
 
 // call the function we just defined using the correct variable
+addGamesToPage(GAMES_JSON);
 // later, we'll call this function using a different list of games
 
 
